79b9f12c179f01f5af09b0cc759e41c6dcfde573
Nisar Ahmed <nisar.tech.world@gmail.com>
2026-05-07 16:40:59 +0500

    Delete src directory

--- src/SMS/Attendance.java
+++
@@ -1,5 +0,0 @@
-package SMS;
-
-public class Attendance {
-
-}
--- src/SmoothImage.java
+++
@@ -1,8 +0,0 @@
-class SmoothImage {
-public static ImageIcon getResizedImage(String path,int width,int height){
-    ImageIcon image = new ImageIcon(
-        new ImageIcon(path).getImage().getScaledInstance(width,height,Image.SCALE_SMOOTH)
-    );
-    return image;
-    }
-}
